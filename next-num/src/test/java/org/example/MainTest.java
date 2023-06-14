2e04974d423df21d92e2f2094c5b053b40302a56
Deyvid Dimitrov <deyvidpdimitrov@gmail.com>
2023-06-14 18:33:06 +0300

    Edited tests

--- next-num/src/test/java/org/example/MainTest.java
+++ next-num/src/test/java/org/example/MainTest.java
@@ -16,4 +16,13 @@ public class MainTest {
         Assertions.assertEquals(Collections.max(result.values()), result.get(1));
         Assertions.assertEquals(Collections.min(result.values()), result.get(-1));
     }
+    @Test
+    public void test_NextNum2() {
+
+        int[] randomNums = {-1, 0, 1, 2, 3};
+        float[] probabilities = {0.25f, 0.2f, 0.11f, 0.09f, 0.35f};
+        Map<Integer, Integer> result = Main.getNextNumber(randomNums, probabilities);
+        Assertions.assertEquals(Collections.max(result.values()), result.get(3));
+        Assertions.assertEquals(Collections.min(result.values()), result.get(2));
+    }
 }
